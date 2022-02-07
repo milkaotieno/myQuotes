24cbc7106f1cc3390b9fe2364dd4a79a3f44785c
milkaotieno <milka.otieno@student.moringaschool.com>
2022-02-07 22:43:02 +0300

    add components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
+import { TestBed, async} from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -26,10 +27,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('myQuotes');
   });
 
-  it('should render title', () => {
+  it('should render title in h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('myQuotes app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome to myQuotes!!');
   });
 });
